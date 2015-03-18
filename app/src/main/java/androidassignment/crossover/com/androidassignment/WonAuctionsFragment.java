de7961fc2c712085e8ec88cbd8fe6c74774e68ac
zeeshan <muhammadzeeshan020@gmail.com>
2015-03-18 21:55:33 +0500

    All fragments designed

--- app/src/main/java/androidassignment/crossover/com/androidassignment/WonAuctionsFragment.java
+++ app/src/main/java/androidassignment/crossover/com/androidassignment/WonAuctionsFragment.java
@@ -5,15 +5,21 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 public class WonAuctionsFragment extends Fragment {
-
+    String color_names[] = {"red", "green", "blue", "yellow", "pink", "brown"};
+    Integer image_id[] = {R.drawable.ic_launcher, R.drawable.ic_launcher,
+            R.drawable.ic_launcher, R.drawable.ic_launcher,
+            R.drawable.ic_launcher, R.drawable.ic_launcher};
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 
 		View rootView = inflater.inflate(R.layout.fragment_won_auctions, container, false);
-		
+        CustomActiveAuctionlistadapter adapter = new CustomActiveAuctionlistadapter(getActivity(), image_id, color_names);
+        ListView lv = (ListView) rootView.findViewById(R.id.listView);
+        lv.setAdapter(adapter);
 		return rootView;
 	}
 }
