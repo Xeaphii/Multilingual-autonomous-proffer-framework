4d2f5f54bdbec607b6e017c3bc7b6329024c2b01
zeeshan <muhammadzeeshan020@gmail.com>
2015-03-18 17:54:04 +0500

    Tabs added

---
+++ app/src/main/java/androidassignment/crossover/com/androidassignment/ActiveAuctionsFragment.java
@@ -0,0 +1,19 @@
+package androidassignment.crossover.com.androidassignment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ActiveAuctionsFragment extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+
+		View rootView = inflater.inflate(R.layout.fragment_active_auctions, container, false);
+		
+		return rootView;
+	}
+}
---
+++ app/src/main/java/androidassignment/crossover/com/androidassignment/WonAuctionsFragment.java
@@ -0,0 +1,19 @@
+package androidassignment.crossover.com.androidassignment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class WonAuctionsFragment extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+
+		View rootView = inflater.inflate(R.layout.fragment_won_auctions, container, false);
+		
+		return rootView;
+	}
+}
